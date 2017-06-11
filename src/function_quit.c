701aa13366bbde8a62bd6ea93139a586a7060c92
Jean Adrien Domage <jeanadrien.domage@epitech.eu>
2017-06-11 22:19:32 +0200

    patch quit

--- src/function_quit.c
+++ src/function_quit.c
@@ -5,7 +5,7 @@
 ** Login   <jeanadrien.domage@epitech.eu>
 ** 
 ** Started on  Thu Jun  8 00:59:02 2017 Jean-Adrien Domage
-** Last update Thu Jun  8 01:10:11 2017 Jean-Adrien Domage
+** Last update Sun Jun 11 22:18:15 2017 Jean-Adrien Domage
 */
 
 #include <stdio.h>
@@ -16,8 +16,11 @@ int	function_quit(t_server *serv, t_peer *peer, t_querry *qry)
 {
   if (qry->av[1])
     dprintf(peer->fd, "%s\r\n", qry->av[1]);
-  else
+  else if (peer->pseudo)
     dprintf(peer->fd, "%s\r\n", peer->pseudo);
+  else
+    dprintf(peer->fd, "Anonymous\r\n");
+
   logout(serv, peer);
   return (0);
 }
